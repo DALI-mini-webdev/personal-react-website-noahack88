6c3aa0a36139faf77475f70c26784c9a1d715428
noahack88 <nackerma@bates.edu>
2021-02-02 23:42:45 -0500

    add nav bar

---
+++ personalsite/src/components/Navbar/MenuItems.js
@@ -0,0 +1,23 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Acomplishments',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+
+] 
